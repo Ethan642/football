21d0f299eb363ce9b28e3351d335b41b4e8e3a51
Ethan642 <ethan.323092@nv.ccsd.net>
2023-02-28 10:14:30 -0800

    player update
    player stuff

---
+++ Sample Project/Assets/Scripts/Football.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Football : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void throwFuﬂball(Vectoe3 direction, float Power)
+    {
+        transform.parent = null;
+        transform.LookAt(direction);
+        rigidbody.AddForce(direction * Power * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Sample Project/Assets/Scripts/PlayerController.cs
+++ Sample Project/Assets/Scripts/PlayerController.cs
@@ -9,8 +9,10 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     public float RunSpeed = 10;
     public Transform groundCheck;
+    public Transform Third;
     public Camera cam;
     public LayerMask groundMask;
+    public float MouseSensitivity;
 
     float x;
     float y;
@@ -27,20 +29,26 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        x += Input.GetAxisRaw("Mouse X");
-        y = Mathf.Clamp(y + Input.GetAxisRaw("Mouse Y"), -90, 90);
+        x += Input.GetAxisRaw("Mouse X") * MouseSensitivity * Time.deltaTime;
+        y = Mathf.Clamp(y - Input.GetAxisRaw("Mouse Y") * MouseSensitivity * Time.deltaTime, -90, 90);
         bool isGrounded = Physics.CheckSphere(groundCheck.position, .4f, groundMask);
 
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
+
+
         if (direction.magnitude >= .1f)
         {
-            controller.Move(direction * RunSpeed * Time.deltaTime);
+            controller.Move((direction + cam.transform.forward) * RunSpeed * Time.deltaTime);
         }
+
+        Third.rotation = Quaternion.Euler(y, x, 0f);
+
+        Gravity *= -2;
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            Gravity += 20;
+            Gravity = 20;
         }
     }
 }
