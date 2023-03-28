c9bc2c45c567360e1c8e667983419693304106f0
Ethan642 <ethan.323092@nv.ccsd.net>
2023-03-28 09:34:52 -0700

    test

--- Sample Project/Assets/Scripts/Football.cs
+++ Sample Project/Assets/Scripts/Football.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Football : MonoBehaviour
 {
     Rigidbody rigidbody;
+    Transform third;
+    public Transform player;
     // Start is called before the first frame update
 
     void Start()
@@ -12,13 +14,18 @@ public class Football : MonoBehaviour
         rigidbody = GetComponent<Rigidbody>();
     }
 
-    public void throwBall(Vector3 direction, float Power)
+    public void throwBall(Vector3 direction, float Power, Transform third2)
     {
         rigidbody.isKinematic = false;
         transform.parent = null;
         
         transform.LookAt(direction);
         
+        if (third2)
+        {
+            third = third2;
+        }
+
         //transform.rotation = Quaternion.Euler(transform.rotation.x - 90f, transform.rotation.y, transform.rotation.z);
         rigidbody.AddForce(direction * Power, ForceMode.Impulse);
     }
@@ -26,8 +33,16 @@ public class Football : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
+        if (third)
+        {
+            third.parent = player;
+            third.localPosition = Vector3.zero;
+            third.GetComponentInChildren<Camera>().transform.localPosition = new Vector3(0.892f, 1, -2.378f);
+            third = null;
+        }
         if (other.collider.transform.parent && other.collider.transform.parent.tag == "GoodTeam")
         {
+
             rigidbody.isKinematic = true;
             transform.parent = other.collider.transform.parent.GetComponent<FootballController>().footballHolder;
             transform.localPosition = new Vector3(-1.561642e-05f, 0, 4.589558e-05f);
--- Sample Project/Assets/Scripts/FootballController.cs
+++ Sample Project/Assets/Scripts/FootballController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FootballController : MonoBehaviour
 {
     public Transform footballHolder;
+    public Transform third;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,10 @@ public class FootballController : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && footballHolder.GetComponentInChildren<Football>())
         {
             Football football = footballHolder.GetComponentInChildren<Football>();
-            football.throwBall(transform.GetComponentInChildren<Camera>().transform.forward, 50f);
+            third.transform.parent = football.transform;
+            third.transform.localPosition = Vector3.zero;
+            third.GetComponentInChildren<Camera>().transform.localPosition = new Vector3(0, 1, -3);
+            football.throwBall(transform.GetComponentInChildren<Camera>().transform.forward, 50f, third);
         }
     }
 }
